14a7de377c50b36b999f5ce4351b01a2ae72b5f2
thbrown <thomasaustenbrown@gmail.com>
2024-01-09 15:10:28 -0700

    More debug

--- docs/bundle.js
+++ docs/bundle.js
@@ -2745,10 +2745,10 @@
                 text,
                 children
               ),
-            // <span key="text" className={classNames(Classes.BUTTON_TEXT, textClassName)}>
-            //     {text}
-            //     {children}
-            // </span>
+              // <span key="text" className={classNames(Classes.BUTTON_TEXT, textClassName)}>
+              //     {text}
+              //     {children}
+              // </span>
             react__WEBPACK_IMPORTED_MODULE_1__.createElement(
               _icon_icon__WEBPACK_IMPORTED_MODULE_9__.Icon,
               { key: "rightIcon", icon: rightIcon }
@@ -2871,301 +2871,6 @@
           ".Card"
         );
         //# sourceMappingURL=card.js.map
-
-        /***/
-      },
-
-    /***/ "./node_modules/@blueprintjs/core/lib/esm/components/collapse/collapse.js":
-      /*!********************************************************************************!*\
-  !*** ./node_modules/@blueprintjs/core/lib/esm/components/collapse/collapse.js ***!
-  \********************************************************************************/
-      /***/ (
-        __unused_webpack_module,
-        __webpack_exports__,
-        __webpack_require__
-      ) => {
-        "use strict";
-        __webpack_require__.r(__webpack_exports__);
-        /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-          /* harmony export */ AnimationStates: () =>
-            /* binding */ AnimationStates,
-          /* harmony export */ Collapse: () => /* binding */ Collapse,
-          /* harmony export */
-        });
-        /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_2__ =
-          __webpack_require__(
-            /*! tslib */ "./node_modules/tslib/tslib.es6.mjs"
-          );
-        /* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_0__ =
-          __webpack_require__(
-            /*! classnames */ "./node_modules/classnames/index.js"
-          );
-        /* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_0___default =
-          /*#__PURE__*/ __webpack_require__.n(
-            classnames__WEBPACK_IMPORTED_MODULE_0__
-          );
-        /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ =
-          __webpack_require__(/*! react */ "./node_modules/react/index.js");
-        /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default =
-          /*#__PURE__*/ __webpack_require__.n(
-            react__WEBPACK_IMPORTED_MODULE_1__
-          );
-        /* harmony import */ var _common__WEBPACK_IMPORTED_MODULE_3__ =
-          __webpack_require__(
-            /*! ../../common */ "./node_modules/@blueprintjs/core/lib/esm/common/classes.js"
-          );
-        /* harmony import */ var _common__WEBPACK_IMPORTED_MODULE_5__ =
-          __webpack_require__(
-            /*! ../../common */ "./node_modules/@blueprintjs/core/lib/esm/common/abstractPureComponent.js"
-          );
-        /* harmony import */ var _common_props__WEBPACK_IMPORTED_MODULE_4__ =
-          __webpack_require__(
-            /*! ../../common/props */ "./node_modules/@blueprintjs/core/lib/esm/common/props.js"
-          );
-        /*
-         * Copyright 2015 Palantir Technologies, Inc. All rights reserved.
-         *
-         * Licensed under the Apache License, Version 2.0 (the "License");
-         * you may not use this file except in compliance with the License.
-         * You may obtain a copy of the License at
-         *
-         *     http://www.apache.org/licenses/LICENSE-2.0
-         *
-         * Unless required by applicable law or agreed to in writing, software
-         * distributed under the License is distributed on an "AS IS" BASIS,
-         * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-         * See the License for the specific language governing permissions and
-         * limitations under the License.
-         */
-
-        /**
-         * `Collapse` can be in one of six states, enumerated here.
-         * When changing the `isOpen` prop, the following happens to the states:
-         * isOpen={true}  : CLOSED -> OPEN_START -> OPENING -> OPEN
-         * isOpen={false} : OPEN -> CLOSING_START -> CLOSING -> CLOSED
-         */
-        var AnimationStates;
-        (function (AnimationStates) {
-          /**
-           * The body is re-rendered, height is set to the measured body height and
-           * the body Y is set to 0.
-           */
-          AnimationStates[(AnimationStates["OPEN_START"] = 0)] = "OPEN_START";
-          /**
-           * Animation begins, height is set to auto. This is all animated, and on
-           * complete, the state changes to OPEN.
-           */
-          AnimationStates[(AnimationStates["OPENING"] = 1)] = "OPENING";
-          /**
-           * The collapse height is set to auto, and the body Y is set to 0 (so the
-           * element can be seen as normal).
-           */
-          AnimationStates[(AnimationStates["OPEN"] = 2)] = "OPEN";
-          /**
-           * Height has been changed from auto to the measured height of the body to
-           * prepare for the closing animation in CLOSING.
-           */
-          AnimationStates[(AnimationStates["CLOSING_START"] = 3)] =
-            "CLOSING_START";
-          /**
-           * Height is set to 0 and the body Y is at -height. Both of these properties
-           * are transformed, and then after the animation is complete, the state
-           * changes to CLOSED.
-           */
-          AnimationStates[(AnimationStates["CLOSING"] = 4)] = "CLOSING";
-          /**
-           * The contents of the collapse is not rendered, the collapse height is 0,
-           * and the body Y is at -height (so that the bottom of the body is at Y=0).
-           */
-          AnimationStates[(AnimationStates["CLOSED"] = 5)] = "CLOSED";
-        })(AnimationStates || (AnimationStates = {}));
-        /**
-         * Collapse component.
-         *
-         * @see https://blueprintjs.com/docs/#core/components/collapse
-         */
-        var Collapse = /** @class */ (function (_super) {
-          (0, tslib__WEBPACK_IMPORTED_MODULE_2__.__extends)(Collapse, _super);
-          function Collapse() {
-            var _this =
-              (_super !== null && _super.apply(this, arguments)) || this;
-            _this.state = {
-              animationState: _this.props.isOpen
-                ? AnimationStates.OPEN
-                : AnimationStates.CLOSED,
-              height: undefined,
-              heightWhenOpen: undefined,
-            };
-            // The element containing the contents of the collapse.
-            _this.contents = null;
-            _this.contentsRefHandler = function (el) {
-              _this.contents = el;
-              if (_this.contents != null) {
-                var height = _this.contents.clientHeight;
-                _this.setState({
-                  animationState: _this.props.isOpen
-                    ? AnimationStates.OPEN
-                    : AnimationStates.CLOSED,
-                  height: height === 0 ? undefined : "".concat(height, "px"),
-                  heightWhenOpen: height === 0 ? undefined : height,
-                });
-              }
-            };
-            return _this;
-          }
-          Collapse.getDerivedStateFromProps = function (props, state) {
-            var isOpen = props.isOpen;
-            var animationState = state.animationState;
-            if (isOpen) {
-              switch (animationState) {
-                case AnimationStates.OPEN:
-                  // no-op
-                  break;
-                case AnimationStates.OPENING:
-                  // allow Collapse#onDelayedStateChange() to handle the transition here
-                  break;
-                default:
-                  return { animationState: AnimationStates.OPEN_START };
-              }
-            } else {
-              switch (animationState) {
-                case AnimationStates.CLOSED:
-                  // no-op
-                  break;
-                case AnimationStates.CLOSING:
-                  // allow Collapse#onDelayedStateChange() to handle the transition here
-                  break;
-                default:
-                  // need to set an explicit height so that transition can work
-                  return {
-                    animationState: AnimationStates.CLOSING_START,
-                    height: "".concat(state.heightWhenOpen, "px"),
-                  };
-              }
-            }
-            return null;
-          };
-          Collapse.prototype.render = function () {
-            var isContentVisible =
-              this.state.animationState !== AnimationStates.CLOSED;
-            var shouldRenderChildren =
-              isContentVisible || this.props.keepChildrenMounted;
-            var displayWithTransform =
-              isContentVisible &&
-              this.state.animationState !== AnimationStates.CLOSING;
-            var isAutoHeight = this.state.height === "auto";
-            var containerStyle = {
-              height: isContentVisible ? this.state.height : undefined,
-              overflowY: isAutoHeight ? "visible" : undefined,
-              // transitions don't work with height: auto
-              transition: isAutoHeight ? "none" : undefined,
-            };
-            var contentsStyle = {
-              // only use heightWhenOpen while closing
-              transform: displayWithTransform
-                ? "translateY(0)"
-                : "translateY(-".concat(this.state.heightWhenOpen, "px)"),
-              // transitions don't work with height: auto
-              transition: isAutoHeight ? "none" : undefined,
-            };
-            return react__WEBPACK_IMPORTED_MODULE_1__.createElement(
-              this.props.component,
-              {
-                className: classnames__WEBPACK_IMPORTED_MODULE_0___default()(
-                  _common__WEBPACK_IMPORTED_MODULE_3__.COLLAPSE,
-                  this.props.className
-                ),
-                style: containerStyle,
-              },
-              react__WEBPACK_IMPORTED_MODULE_1__.createElement(
-                "div",
-                {
-                  className: _common__WEBPACK_IMPORTED_MODULE_3__.COLLAPSE_BODY,
-                  ref: this.contentsRefHandler,
-                  style: contentsStyle,
-                  "aria-hidden": !isContentVisible,
-                },
-                shouldRenderChildren ? this.props.children : null
-              )
-            );
-          };
-          Collapse.prototype.componentDidMount = function () {
-            this.forceUpdate();
-            // HACKHACK: this should probably be done in getSnapshotBeforeUpdate
-            /* eslint-disable react/no-did-mount-set-state */
-            if (this.props.isOpen) {
-              this.setState({
-                animationState: AnimationStates.OPEN,
-                height: "auto",
-              });
-            } else {
-              this.setState({
-                animationState: AnimationStates.CLOSED,
-                height: "0px",
-              });
-            }
-            /* eslint-disable react/no-did-mount-set-state */
-          };
-          Collapse.prototype.componentDidUpdate = function () {
-            var _this = this;
-            if (this.contents == null) {
-              return;
-            }
-            var transitionDuration = this.props.transitionDuration;
-            var animationState = this.state.animationState;
-            if (animationState === AnimationStates.OPEN_START) {
-              var clientHeight = this.contents.clientHeight;
-              this.setState({
-                animationState: AnimationStates.OPENING,
-                height: "".concat(clientHeight, "px"),
-                heightWhenOpen: clientHeight,
-              });
-              this.setTimeout(function () {
-                return _this.onDelayedStateChange();
-              }, transitionDuration);
-            } else if (animationState === AnimationStates.CLOSING_START) {
-              var clientHeight_1 = this.contents.clientHeight;
-              this.setTimeout(function () {
-                return _this.setState({
-                  animationState: AnimationStates.CLOSING,
-                  height: "0px",
-                  heightWhenOpen: clientHeight_1,
-                });
-              });
-              this.setTimeout(function () {
-                return _this.onDelayedStateChange();
-              }, transitionDuration);
-            }
-          };
-          Collapse.prototype.onDelayedStateChange = function () {
-            switch (this.state.animationState) {
-              case AnimationStates.OPENING:
-                this.setState({
-                  animationState: AnimationStates.OPEN,
-                  height: "auto",
-                });
-                break;
-              case AnimationStates.CLOSING:
-                this.setState({ animationState: AnimationStates.CLOSED });
-                break;
-              default:
-                break;
-            }
-          };
-          Collapse.displayName = "".concat(
-            _common_props__WEBPACK_IMPORTED_MODULE_4__.DISPLAYNAME_PREFIX,
-            ".Collapse"
-          );
-          Collapse.defaultProps = {
-            component: "div",
-            isOpen: false,
-            keepChildrenMounted: false,
-            transitionDuration: 200,
-          };
-          return Collapse;
-        })(_common__WEBPACK_IMPORTED_MODULE_5__.AbstractPureComponent);
-
-        //# sourceMappingURL=collapse.js.map
 
         /***/
       },
@@ -12030,6 +11735,7 @@
             return (0, _utils_toWebM__WEBPACK_IMPORTED_MODULE_0__["default"])(
               images.map((image, index) => {
                 try {
+                  alert(("PROCESSING FRAME " + index).substring(0, 50));
                   alert(("DECODING F " + image).substring(0, 50));
                   alert(("DECODING L " + image.length).substring(0, 50));
                   alert(("DECODING S " + image.slice(23)).substring(0, 50));
@@ -12993,10 +12699,6 @@
           __webpack_require__(
             /*! @blueprintjs/core */ "./node_modules/@blueprintjs/core/lib/esm/components/button/buttons.js"
           );
-        /* harmony import */ var _blueprintjs_core__WEBPACK_IMPORTED_MODULE_8__ =
-          __webpack_require__(
-            /*! @blueprintjs/core */ "./node_modules/@blueprintjs/core/lib/esm/components/collapse/collapse.js"
-          );
         /* harmony import */ var _blueprintjs_icons__WEBPACK_IMPORTED_MODULE_6__ =
           __webpack_require__(
             /*! @blueprintjs/icons */ "./node_modules/@blueprintjs/icons/lib/esm/generated/components/chevron-down.js"
@@ -13360,37 +13062,40 @@
                     " advanced options"
                   )
                 : null,
-              /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0___default().createElement(
-                _blueprintjs_core__WEBPACK_IMPORTED_MODULE_8__.Collapse,
-                {
-                  isOpen: areAdvancedOptionsEnabled,
-                },
-                /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_0___default().createElement(
-                  "div",
-                  {
-                    style: {
-                      height: "25vh",
-                      overflow: "auto",
-                      background: "#2d72d212",
-                      //lightblue
-                      paddingTop: "10px",
-                      borderRadius: "3px",
-                    },
-                  },
-                  areAdvancedOptionsEnabled
-                    ? generateUIForCameraCapabilities(
-                        props.supportedCameraCapabilities,
-                        props.cameraSettings
-                      )
-                    : null
-                )
-              )
+              areAdvancedOptionsEnabled
+                ? generateUIForCameraCapabilities(
+                    props.supportedCameraCapabilities,
+                    props.cameraSettings
+                  )
+                : null
             );
           } catch (e) {
             console.log(e);
             alert(e + " - " + e.stack);
           }
         }
+
+        /*
+
+        <Collapse isOpen={areAdvancedOptionsEnabled}>
+          <div
+            style={{
+              height: "25vh",
+              overflow: "auto",
+              background: "#2d72d212", //lightblue
+              paddingTop: "10px",
+              borderRadius: "3px",
+            }}
+          >
+            {areAdvancedOptionsEnabled
+              ? generateUIForCameraCapabilities(
+                  props.supportedCameraCapabilities,
+                  props.cameraSettings
+                )
+              : null}
+          </div>
+        </Collapse>
+        */
 
         /***/
       },
@@ -18124,7 +17829,7 @@
                     switch ((_context.prev = _context.next)) {
                       case 0:
                         _context.prev = 0;
-                        alert("Processing " + inputFrames);
+                        alert(("Processing " + inputFrames).substring(0, 150));
                         videoBlob =
                           ts_whammy_src_libs__WEBPACK_IMPORTED_MODULE_0__[
                             "default"
